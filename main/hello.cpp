bcce9a623f6bb44bd9031f91d59e0a224f9db00d
Yuvan <zkcdxhd@gmail.com>
2023-02-15 19:59:16 +0530

    first commit

---
+++ main/hello.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, PES2UG20CS400\n";
+  cout << "Hello, Jenkins\n";
+  cout << "Running successfully\n";
+  return 0;
+}
