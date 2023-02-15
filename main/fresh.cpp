89bb2189eaf9ec8165d80f919bbe81eee3ddf1db
Yuvan <zkcdxhd@gmail.com>
2023-02-15 21:14:50 +0530

    Added new .cpp file

---
+++ main/fresh.cpp
@@ -0,0 +1,10 @@
+//File: fresh.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, PES2UG20CS400\n";
+  cout << "This is a fresh file\n";
+  cout << "Pipeline test\n";
+  return 0;
+}
